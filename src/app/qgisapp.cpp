7e5ff448d40e98726d33beea4aea267f5990c664
Giuseppe Sucameli <brush.tyler@gmail.com>
2012-08-21 17:25:06 +0200

    Project macros: do not display the warning bar when there are no macros in the project, decrease warning bar message level for macros

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -458,6 +458,7 @@ QgisApp::QgisApp( QSplashScreen *splash, bool restorePlugins, QWidget * parent, 
   QWidget *centralWidget = this->centralWidget();
   QGridLayout *centralLayout = new QGridLayout( centralWidget );
   centralWidget->setLayout( centralLayout );
+  centralLayout->setContentsMargins( 0, 0, 0, 0 );
 
   // "theMapCanvas" used to find this canonical instance later
   mMapCanvas = new QgsMapCanvas( centralWidget, "theMapCanvas" );
@@ -3233,9 +3234,9 @@ void QgisApp::fileOpen()
       {
         enableProjectMacros();
       }
-      else
+      else if ( !QgsProject::instance()->readEntry( "Macros", "/pythonCode", QString::null ).isEmpty() )
       {
-        mInfoBar->pushWidget( mMacrosWarn, 2 );
+        mInfoBar->pushWidget( mMacrosWarn, 1 );
       }
     }
 
